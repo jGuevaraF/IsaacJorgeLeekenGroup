28c46b1c7dfbba681707c156a7d877376b169d54
jGuevaraF <jguevaraflores3@gmail.com>
2023-10-13 13:09:13 -0600

    master manipulado

---
+++ ML/Entidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Entidad
+    {
+        public int Id { get; set; }
+        public string Estado { get; set; }
+        public List<object> Entidades { get; set; }
+    }
+}
