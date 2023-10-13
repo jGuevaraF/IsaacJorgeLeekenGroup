bf7da78d6a732c1563b16da90011192fd3ccd808
jGuevaraF <jguevaraflores3@gmail.com>
2023-10-13 13:31:45 -0600

    DL corregido

--- BL/Empleado.cs
+++ BL/Empleado.cs
@@ -8,5 +8,23 @@ namespace BL
 {
     public class Empleado
     {
+        public ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.IsaacJorgeLeekenGroupEntities context = new DL.IsaacJorgeLeekenGroupEntities())
+                {
+                    
+                }
+
+            } catch (Exception ex) {
+                result.Correct = false;
+                result.ErrorMessage = ex.InnerException.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+        }
     }
 }
