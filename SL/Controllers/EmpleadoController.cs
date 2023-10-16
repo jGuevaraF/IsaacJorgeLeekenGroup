2df63c62753198b1e6e7527d17a83b39a083d0b9
jGuevaraF <jguevaraflores3@gmail.com>
2023-10-16 15:11:09 -0600

    Capa SL Terminada

--- SL/Controllers/EmpleadoController.cs
+++ SL/Controllers/EmpleadoController.cs
@@ -10,7 +10,8 @@ namespace SL.Controllers
     [RoutePrefix("api/empleado")]
     public class EmpleadoController : ApiController
     {
-      
+        [Route("")]
+        [HttpGet]
         public IHttpActionResult GetAll()
         {
             ML.Result result = BL.Empleado.GetAll();
@@ -23,5 +24,36 @@ namespace SL.Controllers
                 return Content(HttpStatusCode.BadRequest, result);
             }
         }
+
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult Add(ML.Empleado empleado)
+        {
+            ML.Result result = BL.Empleado.Add(empleado);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
+
+        [Route("{idEmpleado}")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int idEmpleado)
+        {
+            ML.Result result = BL.Empleado.Delete(idEmpleado);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
+
     }
 }
